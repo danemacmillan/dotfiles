56d866985ec64033f0f22e50f642834159956986
Dane MacMillan <work@danemacmillan.com>
2015-02-04 11:02:00 -0500

    Add update command for CentOS.

--- .nix
+++ .nix
@@ -1,3 +1,6 @@
+# Aliases for CentOS
+alias update="yum -y update"
+
 # Add tab completion for many Bash commands, including the
 # ability to add extended Git info in PS1.
 if [ -f /etc/bash_completion ]; then
