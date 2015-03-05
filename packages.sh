b70b1649c66d473090a39f80089b3160e9142590
Dane MacMillan <work@danemacmillan.com>
2015-03-05 03:02:59 -0500

    Refactor packages to loop through array.

--- packages.sh
+++ packages.sh
@@ -1,64 +1,50 @@
 #!/usr/bin/env bash
-##
-# packages.sh
+
+
 #
-# This file gets sourced by bootstrap.sh. It installs all packages found in
-# the packages directory, dependent on the package management system available.
+# External dependencies
+#
+
+# Homebrew
+echo -e "${BLUE}${BOLD}Downloading external dependencies, if any.${RESET}"
+# Get homebrew and some niceties.
+if ! hash brew; then
+	echo "${GREEN}Installing homebrew.${RESET}"
+	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Vundle
+echo -e "${GREEN}Installing Vim Vundle and plugins. This will take a few seconds.${RESET}"
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim &> /dev/null
+# Install Vim plugins defined in .vimrc file.
+# Depending on the location that Vundler installs a plugin, there
+# may be an authentication prompt for username and password.
+vim +PluginInstall +qall 2&> /dev/null
 
 
-# Clone git repos before anything else.
+#
+# Packages
+#
 
-
-
-
-# Install packages from OS' package manager.
+# Install packages from OS package manager.
 DOTFILES_PACKAGES_DIR="$HOME/.packages"
 case "$DOTFILES_PACKAGE_MANAGER" in
 	brew)
-		# brew tap
-		DOTFILES_PACKAGES_CURRENT='tap'
-		DOTFILES_PACKAGES_FILE="$DOTFILES_PACKAGES_DIR/$DOTFILES_PACKAGES_CURRENT"
-		DOTFILES_PACKAGES_MD5_OLD=$DOTFILES_PACKAGES_MD5_TAP
-		DOTFILES_PACKAGES_MD5_NEW=$(calculate_md5_hash "$DOTFILES_PACKAGES_FILE")
-		if [[ "$DOTFILES_PACKAGES_MD5_OLD" != "$DOTFILES_PACKAGES_MD5_NEW" ]]; then
+		# Use three distinct packages for brew.
+		for package_name in "tap brew cask"; do
+			DOTFILES_PACKAGES_FILE="$DOTFILES_PACKAGES_DIR/$package_name"
+			DOTFILES_PACKAGES_MD5_OLD=$DOTFILES_PACKAGES_MD5_TAP
+			DOTFILES_PACKAGES_MD5_NEW=$(calculate_md5_hash "$DOTFILES_PACKAGES_FILE")
 
-			echo -e "${BLUE}${BOLD}Installing new ${GREEN}${REVERSE} $DOTFILES_PACKAGE_MANAGER $DOTFILES_PACKAGES_CURRENT ${RESET}${BLUE}${BOLD} packages.${RESET}"
-			while read line; do
-				if ! hash $line ; then
-					echo $line
-				fi
-			done < $DOTFILES_PACKAGES_DIR/$DOTFILES_PACKAGES_CURRENT
-		fi
-
-		# brew
-		DOTFILES_PACKAGES_CURRENT=$DOTFILES_PACKAGE_MANAGER
-		DOTFILES_PACKAGES_FILE="$DOTFILES_PACKAGES_DIR/$DOTFILES_PACKAGE_MANAGER"
-		DOTFILES_PACKAGES_MD5_OLD=$DOTFILES_PACKAGES_MD5_BREW
-		DOTFILES_PACKAGES_MD5_NEW=$(calculate_md5_hash "$DOTFILES_PACKAGES_FILE")
-		if [[ "$DOTFILES_PACKAGES_MD5_OLD" != "$DOTFILES_PACKAGES_MD5_NEW" ]]; then
-
-			echo -e "${BLUE}${BOLD}Installing new ${GREEN}${REVERSE} $DOTFILES_PACKAGE_MANAGER ${RESET}${BLUE}${BOLD} packages.${RESET}"
-			while read line; do
-				if ! hash $line ; then
-					echo $line
-				fi
-			done < "$DOTFILES_PACKAGES_DIR/$DOTFILES_PACKAGE_MANAGER"
-		fi
-
-		# brew cask
-		DOTFILES_PACKAGES_CURRENT='cask'
-		DOTFILES_PACKAGES_FILE="$DOTFILES_PACKAGES_DIR/$DOTFILES_PACKAGES_CURRENT"
-		DOTFILES_PACKAGES_MD5_OLD=$DOTFILES_PACKAGES_MD5_CASK
-		DOTFILES_PACKAGES_MD5_NEW=$(calculate_md5_hash "$DOTFILES_PACKAGES_FILE")
-		if [[ "$DOTFILES_PACKAGES_MD5_OLD" != "$DOTFILES_PACKAGES_MD5_NEW" ]]; then
-
-			echo -e "${BLUE}${BOLD}Installing new ${GREEN}${REVERSE} $DOTFILES_PACKAGE_MANAGER $DOTFILES_PACKAGES_CURRENT ${RESET}${BLUE}${BOLD} packages.${RESET}"
-			while read line; do
-				if ! hash $line ; then
-					echo $line
-				fi
-			done < "$DOTFILES_PACKAGES_DIR/$DOTFILES_PACKAGES_CURRENT"
-		fi
+			if [[ "$DOTFILES_PACKAGES_MD5_OLD" != "$DOTFILES_PACKAGES_MD5_NEW" ]]; then
+				echo -e "${BLUE}${BOLD}Installing new ${GREEN}${REVERSE} $DOTFILES_PACKAGE_MANAGER $package_name ${RESET}${BLUE}${BOLD} packages.${RESET}"
+				while read line; do
+					if ! hash $line ; then
+						echo $line
+					fi
+				done < $DOTFILES_PACKAGES_DIR/$package_name
+			fi
+		done
 		;;
 
 	yum)
@@ -68,46 +54,3 @@ case "$DOTFILES_PACKAGE_MANAGER" in
 		;;
 esac
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-##
-# Git repos
-
-
-# Install Vundler for Vim.
-#echo -e "Installing Vim Vundler and plugins. This may take a few seconds."
-#git clone git@github.com:gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim &> /dev/null
-#git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim &> /dev/null
-# Install Vim plugins defined in .vimrc file.
-# Depending on the location that Vundler installs a plugin, there
-# may be an authentication prompt for username and password.
-#vim +PluginInstall +qall 2&> /dev/null
-#vim +PluginInstall +qall 2&>/dev/null
