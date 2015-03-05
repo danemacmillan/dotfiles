ef3bf82cac01df1e0043fd13d6ddd71679bf76ac
Dane MacMillan <work@danemacmillan.com>
2015-03-05 04:07:52 -0500

    Encapsulate dotfiles package installer in function.

--- packages.sh
+++ packages.sh
@@ -26,63 +26,73 @@ echo -e "${GREEN}Installing Vim Vundle and plugins. This will take a few seconds
 # Packages
 #
 
-# Install packages from OS package manager.
-DOTFILES_PACKAGES_DIR="$HOME/.packages"
-case "$DOTFILES_PACKAGE_MANAGER" in
-	brew)
-		# Use three distinct packages for brew.
-		for package_name in "tap" "brew" "cask"; do
-			DOTFILES_PACKAGES_FILE="$DOTFILES_PACKAGES_DIR/$package_name"
-			DOTFILES_PACKAGES_MD5_NEW=$(calculate_md5_hash "$DOTFILES_PACKAGES_FILE")
+##
+# Install packages from files, irregardless of OS.
+#
+# This function depends on the existence of package files in the `.packages`
+# directory. Ensure that the name will correspond with the cases here.
+dotfiles_package_installer()
+{
+	# Install packages from OS package manager.
+	DOTFILES_PACKAGES_DIR="$HOME/.packages"
+	case "$DOTFILES_PACKAGE_MANAGER" in
+		brew)
+			# Use three distinct packages for brew.
+			for package_name in "tap" "brew" "cask"; do
+				DOTFILES_PACKAGES_FILE="$DOTFILES_PACKAGES_DIR/$package_name"
+				DOTFILES_PACKAGES_MD5_NEW=$(calculate_md5_hash "$DOTFILES_PACKAGES_FILE")
 
-			case "$package_name" in
-				tap)
-					DOTFILES_PACKAGES_MD5_OLD=$DOTFILES_PACKAGES_MD5_TAP
-					DOTFILES_PACKAGE_MANAGER_COMMAND="brew tap"
-					DOTFILES_PACKAGE_MANAGER_COMMAND_LIST="brew tap"
-					;;
-				brew)
-					DOTFILES_PACKAGES_MD5_OLD=$DOTFILES_PACKAGES_MD5_BREW
-					DOTFILES_PACKAGE_MANAGER_COMMAND="brew install"
-					DOTFILES_PACKAGE_MANAGER_COMMAND_LIST="brew list -1"
-					;;
-				cask)
-					DOTFILES_PACKAGES_MD5_OLD=$DOTFILES_PACKAGES_MD5_CASK
-					DOTFILES_PACKAGE_MANAGER_COMMAND="brew cask install"
-					DOTFILES_PACKAGE_MANAGER_COMMAND_LIST="brew cask list -1"
-			esac
+				case "$package_name" in
+					tap)
+						DOTFILES_PACKAGES_MD5_OLD=$DOTFILES_PACKAGES_MD5_TAP
+						DOTFILES_PACKAGE_MANAGER_COMMAND="brew tap"
+						DOTFILES_PACKAGE_MANAGER_COMMAND_LIST="brew tap"
+						;;
+					brew)
+						DOTFILES_PACKAGES_MD5_OLD=$DOTFILES_PACKAGES_MD5_BREW
+						DOTFILES_PACKAGE_MANAGER_COMMAND="brew install"
+						DOTFILES_PACKAGE_MANAGER_COMMAND_LIST="brew list -1"
+						;;
+					cask)
+						DOTFILES_PACKAGES_MD5_OLD=$DOTFILES_PACKAGES_MD5_CASK
+						DOTFILES_PACKAGE_MANAGER_COMMAND="brew cask install"
+						DOTFILES_PACKAGE_MANAGER_COMMAND_LIST="brew cask list -1"
+				esac
+
+				if [[ "$DOTFILES_PACKAGES_MD5_OLD" != "$DOTFILES_PACKAGES_MD5_NEW" ]]; then
+					echo -e "${BLUE}${BOLD}Installing new ${GREEN}${REVERSE} $DOTFILES_PACKAGE_MANAGER $package_name ${RESET}${BLUE}${BOLD} packages.${RESET}"
+					while read line; do
+						# Note: using a hash check is insufficient, as not all packages
+						# are installed as an executable CLI tool.
+						if ! $DOTFILES_PACKAGE_MANAGER_COMMAND_LIST | grep -q "^${line}\$"; then
+							$DOTFILES_PACKAGE_MANAGER_COMMAND $line
+						fi
+					done < $DOTFILES_PACKAGES_DIR/$package_name
+				fi
+			done
+			;;
+
+		yum)
+			DOTFILES_PACKAGES_FILE="$DOTFILES_PACKAGES_DIR/$DOTFILES_PACKAGE_MANAGER"
+			DOTFILES_PACKAGES_MD5_NEW=$(calculate_md5_hash "$DOTFILES_PACKAGES_FILE")
+			DOTFILES_PACKAGES_MD5_OLD=$DOTFILES_PACKAGES_MD5_YUM
+			DOTFILES_PACKAGE_MANAGER_COMMAND="yum -y install"
+			DOTFILES_PACKAGE_MANAGER_COMMAND_LIST="yum list installed"
 
 			if [[ "$DOTFILES_PACKAGES_MD5_OLD" != "$DOTFILES_PACKAGES_MD5_NEW" ]]; then
-				echo -e "${BLUE}${BOLD}Installing new ${GREEN}${REVERSE} $DOTFILES_PACKAGE_MANAGER $package_name ${RESET}${BLUE}${BOLD} packages.${RESET}"
+				echo -e "${BLUE}${BOLD}Installing new ${GREEN}${REVERSE} $DOTFILES_PACKAGE_MANAGER ${RESET}${BLUE}${BOLD} packages.${RESET}"
 				while read line; do
-					# Note: using a hash check is insufficient, as not all packages
-					# are installed as an executable CLI tool.
-					if ! $DOTFILES_PACKAGE_MANAGER_COMMAND_LIST | grep -q "^${line}\$"; then
+					if ! $DOTFILES_PACKAGE_MANAGER_COMMAND_LIST $line | grep -q "^${line}\$"; then
 						$DOTFILES_PACKAGE_MANAGER_COMMAND $line
 					fi
-				done < $DOTFILES_PACKAGES_DIR/$package_name
+				done < $DOTFILES_PACKAGES_DIR/$DOTFILES_PACKAGE_MANAGER
 			fi
-		done
-		;;
+			;;
 
-	yum)
-		DOTFILES_PACKAGES_FILE="$DOTFILES_PACKAGES_DIR/$DOTFILES_PACKAGE_MANAGER"
-		DOTFILES_PACKAGES_MD5_NEW=$(calculate_md5_hash "$DOTFILES_PACKAGES_FILE")
-		DOTFILES_PACKAGES_MD5_OLD=$DOTFILES_PACKAGES_MD5_YUM
-		DOTFILES_PACKAGE_MANAGER_COMMAND="yum -y install"
-		DOTFILES_PACKAGE_MANAGER_COMMAND_LIST="yum list installed"
+		apt-get)
+			;;
+	esac
+}
 
-		if [[ "$DOTFILES_PACKAGES_MD5_OLD" != "$DOTFILES_PACKAGES_MD5_NEW" ]]; then
-			echo -e "${BLUE}${BOLD}Installing new ${GREEN}${REVERSE} $DOTFILES_PACKAGE_MANAGER ${RESET}${BLUE}${BOLD} packages.${RESET}"
-			while read line; do
-				if ! $DOTFILES_PACKAGE_MANAGER_COMMAND_LIST $line | grep -q "^${line}\$"; then
-					$DOTFILES_PACKAGE_MANAGER_COMMAND $line
-				fi
-			done < $DOTFILES_PACKAGES_DIR/$DOTFILES_PACKAGE_MANAGER
-		fi
-		;;
-
-	apt-get)
-		;;
-esac
-
+# Execute the function.
+dotfiles_package_installer
