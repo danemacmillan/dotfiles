1acc60dc1d65f49aebf6730f4bce8e72ce408feb
Dane MacMillan <work@danemacmillan.com>
2015-03-06 15:37:59 -0500

    Abstract md5 checker and packager manager type checker into separate functions. Create yum plug.

--- packages.sh
+++ packages.sh
@@ -7,28 +7,60 @@
 # individual pluggable package installers.
 #
 
+# Determine paths for installing packages.
+DOTFILES_PACKAGES_DIR=("$HOME/.dotfiles/.dfpackages")
+if [ -d "$HOME/.dfpackages" ]; then
+	DOTFILES_PACKAGES_DIR+=("$HOME/.dfpackages")
+fi
+export DOTFILES_PACKAGES_DIR
+
 ##
-# Helper function for verifying dotfiles' package changes and defining
-# the available package types.
-# TODO: add check for user-defined dfpackages directory in home directory.
-# TODO: create array of support package types, e.g., brew, git, wget, so that
-# multiple package types can be operated on.
+# Get list of available package manager types, which the installer will
+# operate against.
+dotfiles_managers_get()
+{
+	local package_managers=()
+
+	if hash git 2>/dev/null; then
+		package_managers+=("git")
+	fi
+
+	if hash wget 2>/dev/null; then
+		package_managers+=("brew")
+	fi
+
+	if hash brew 2>/dev/null; then
+		package_managers+=("brew")
+	fi
+
+	if hash yum 2>/dev/null; then
+		package_managers+=("yum")
+	fi
+
+	if hash apt-get 2>/dev/null; then
+		package_managers+=("apt-get")
+	fi
+
+	export DOTFILES_PACKAGER_MANAGERS=$package_managers
+}
+
+##
+# Verify and store the MD5 hashes of all packages being used by dotfiles.
 dotfiles_packages_verify()
 {
-	local packages_directory="$HOME/.dotfiles/.dfpackages"
-	export DOTFILES_PACKAGE_MANAGER=""
-	if hash brew 2>/dev/null; then
-		export DOTFILES_PACKAGE_MANAGER="brew"
-		export DOTFILES_PACKAGES_MD5_TAP=$(calculate_md5_hash "$packages_directory/tap")
-		export DOTFILES_PACKAGES_MD5_BREW=$(calculate_md5_hash "$packages_directory/$DOTFILES_PACKAGE_MANAGER")
-		export DOTFILES_PACKAGES_MD5_CASK=$(calculate_md5_hash "$packages_directory/cask")
-	elif hash yum 2>/dev/null; then
-		export DOTFILES_PACKAGE_MANAGER="yum"
-		export DOTFILES_PACKAGES_MD5_YUM=$(calculate_md5_hash "$packages_directory/$DOTFILES_PACKAGE_MANAGER")
-	elif hash apt-get 2>/dev/null; then
-		export DOTFILES_PACKAGE_MANAGER="apt-get"
-		export DOTFILES_PACKAGES_MD5_APTGET=$(calculate_md5_hash "$packages_directory/$DOTFILES_PACKAGE_MANAGER")
-	fi
+	for packages_dir in ${DOTFILES_PACKAGES_DIR[@]}; do
+		if [ -d $packages_dir ]; then
+			if hash brew 2>/dev/null; then
+				export DOTFILES_PACKAGES_MD5_TAP=$(calculate_md5_hash "$packages_dir/tap")
+				export DOTFILES_PACKAGES_MD5_BREW=$(calculate_md5_hash "$packages_dir/$DOTFILES_PACKAGE_MANAGER")
+				export DOTFILES_PACKAGES_MD5_CASK=$(calculate_md5_hash "$packages_dir/cask")
+			elif hash yum 2>/dev/null; then
+				export DOTFILES_PACKAGES_MD5_YUM=$(calculate_md5_hash "$packages_dir/$DOTFILES_PACKAGE_MANAGER")
+			elif hash apt-get 2>/dev/null; then
+				export DOTFILES_PACKAGES_MD5_APTGET=$(calculate_md5_hash "$packages_dir/$DOTFILES_PACKAGE_MANAGER")
+			fi
+		fi
+	done
 }
 
 ##
@@ -37,8 +69,8 @@ dotfiles_packages_verify()
 # TODO: add check for user's user-defined packages in home directory.
 dotfiles_packages_install_brew()
 {
-	local package="$1"
-	local packages_dir="$HOME/.dotfiles/.dfpackages"
+	local package="brew"
+	local packages_dir="$1"
 
 	if [ -d $packages_dir ]; then
 		# Use three distinct packages for brew.
@@ -70,7 +102,7 @@ dotfiles_packages_install_brew()
 			if [[ "$packages_md5_old" != "$packages_md5_new" ]]; then
 				echo -e "${BLUE}${BOLD}Installing new ${GREEN}${REVERSE} $package_name ${RESET}${BLUE}${BOLD} packages.${RESET}"
 				while read line; do
-					# Ssing a hash check is insufficient, as not all packages are
+					# A hash check is insufficient, as not all packages are
 					# installed as an executable CLI tool. This is why check is against
 					# list of packages.
 					if ! $package_manager_command_list | grep -q "^${line}\$"; then
@@ -81,6 +113,35 @@ dotfiles_packages_install_brew()
 			fi
 		done
 	fi
+}
+
+##
+# Pluggable package installer for yum executed by dotfiles_packages_installer.
+dotfiles_packages_install_yum()
+{
+	local package="yum"
+	local packages_dir="$1"
+
+	if [ -d $packages_dir ]; then
+		local packages_file="$packages_dir/$subpackage"
+		local packages_md5_new=$(calculate_md5_hash "$packages_file")
+		local packages_md5_old=$DOTFILES_PACKAGES_MD5_YUM
+		local package_manager_command="yum -y install"
+		local package_manager_command_list="yum list installed"
+		local package_name="$package"
+
+		if [[ "$packages_md5_old" != "$packages_md5_new" ]]; then
+			echo -e "${BLUE}${BOLD}Installing new ${GREEN}${REVERSE} $package_name ${RESET}${BLUE}${BOLD} packages.${RESET}"
+			while read line; do
+				# A hash check is insufficient, as not all packages are
+				# installed as an executable CLI tool. This is why check is against
+				# list of packages.
+				if ! $package_manager_command_list | grep -q "^${line}\$"; then
+					$package_manager_command $line
+				fi
+			done < $packages_dir/$package
+			yum clean all 2&> /dev/null
+		fi
 }
 
 ##
@@ -121,39 +182,32 @@ dotfiles_packages_install()
 	# any other packages can be installed.
 	dotfiles_packages_install_extras
 
-	# Install packages from OS package manager.
-	# TODO: configure so variable can include git, wget, rpm. This means the
-	# case statement will need to be changed to individual if conditions to
-	# handle multiple systems.
-	case "$DOTFILES_PACKAGE_MANAGER" in
-		brew)
-			dotfiles_packages_install_brew "$DOTFILES_PACKAGE_MANAGER"
-			;;
+	# Install packages from varyious defined package managers.
+	for package_manager in ${DOTFILES_PACKAGE_MANAGERS[@]}; do
+		for packages_dir in ${DOTFILES_PACKAGES_DIR[@]}; do
+			if [ -d $packages_dir ]; then
+				case "$package_manager" in
+					wget)
+						;;
 
-		# TODO: add RPM installs as well. Ensure these subpackages are available
-		# before yum installer is called.
-		yum)
-			DOTFILES_PACKAGES_DIR="$HOME/.dotfiles/.dfpackages"
-			DOTFILES_PACKAGES_FILE="$DOTFILES_PACKAGES_DIR/$DOTFILES_PACKAGE_MANAGER"
-			DOTFILES_PACKAGES_MD5_NEW=$(calculate_md5_hash "$DOTFILES_PACKAGES_FILE")
-			DOTFILES_PACKAGES_MD5_OLD=$DOTFILES_PACKAGES_MD5_YUM
-			DOTFILES_PACKAGE_MANAGER_COMMAND="yum -y install"
-			DOTFILES_PACKAGE_MANAGER_COMMAND_LIST="yum list installed"
+					git)
+						;;
 
-			if [[ "$DOTFILES_PACKAGES_MD5_OLD" != "$DOTFILES_PACKAGES_MD5_NEW" ]]; then
-				echo -e "${BLUE}${BOLD}Installing new ${GREEN}${REVERSE} $DOTFILES_PACKAGE_MANAGER ${RESET}${BLUE}${BOLD} packages.${RESET}"
-				while read line; do
-					if ! $DOTFILES_PACKAGE_MANAGER_COMMAND_LIST $line | grep -q "^${line}\$"; then
-						$DOTFILES_PACKAGE_MANAGER_COMMAND $line
-					fi
-				done < $DOTFILES_PACKAGES_DIR/$DOTFILES_PACKAGE_MANAGER
-				yum clean all 2&> /dev/null
+					brew)
+						dotfiles_packages_install_brew "$packages_dir"
+						;;
+
+					yum)
+						dotfiles_packages_install_yum "$packages_dir"
+						;;
+
+					apt-get)
+						;;
+				esac
 			fi
-			;;
+		done
+	done
 
-		apt-get)
-			;;
-	esac
 }
 
 # Install package depencencies only if install flag passed
