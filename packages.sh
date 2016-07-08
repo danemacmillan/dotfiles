ab3070967b785ea631b2bdda559738120e108470
Dane MacMillan <work@danemacmillan.com>
2016-07-08 18:16:28 -0400

    Allow packages in wheelgroup to install packages.

--- packages.sh
+++ packages.sh
@@ -187,7 +187,7 @@ dotfiles_packages_install_yum()
 		local packages_file="$packages_dir/$package"
 		local packages_md5_new=$(calculate_md5_hash "$packages_file")
 		local packages_md5_old=$(eval echo \$DOTFILES_PACKAGES_MD5_yum_$directory_index)
-		local package_manager_command="yum -y install"
+		local package_manager_command="sudo yum -y install"
 		local package_manager_command_list="rpm -q --nodigest --nosignature"
 		local package_name="$package"
 
