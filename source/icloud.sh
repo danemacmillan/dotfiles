d79bf3b83c963af9662a54f0748941812441b690
Dane MacMillan <work@danemacmillan.com>
2024-08-14 18:15:41 -0400

    Add draft for iCloud Disk technique.
    This create a new bash function that describes the iCloud Disk
    technique, in addition to writing up a basic function to create new
    disks. This is a rough implementation, but has worked very well for
    quite some time.

--- source/icloud.sh
+++ source/icloud.sh
@@ -13,16 +13,115 @@
 # @link https://github.com/danemacmillan/dotfiles
 # @license MIT
 
+##
+# iCloud Disk
+#
+# This is a utility for generating an "iCloud Disk," which is a technique for
+# leveraging iCloud Drive to store disks created on MacOS as sparse bundles,
+# which are more efficient on cloud storage, as they are represented as many
+# small files instead of one large one. For example, a disk that is 500GB and
+# mounted to the filesystem, which then has some of its contained files modified
+# would not result in a 500GB file transfer to iCloud Drive, but only the
+# "bands" that represent the area of the sparse bundle that were edited.
+#
+# The size of a bundle can be customized. By default they are 8MB. The best
+# size of a band depends on what kind of content will be stored on the disk. A
+# disk that will contain many small files (tens or hundreds of thousands) will
+# benefit from a larger band size, as there is a balance that needs to be found
+# between having too many files in iCloud Drive, which will not handle hundreds
+# of thousands of files very gracefully, especially not ones that are constantly
+# changing, as in the use case of writing software, and the need to reduce the
+# overall bandwidth requirements of transferring too much based on relatively
+# small file edits.
+#
+# This utility also by default encrypts the sparse bundle.
+#
+# Valid values for SPARSEBUNDLE range from 2048 to 16777216 sectors (1 MB
+# to 8 GB). These are the size of a sparse band (sparse-band-size), so:
+#
+#  - 1MB band = 2048
+#  - 8MB band = 2048*8 = 16,384 (this is the 8MB default of Disk Utility)
+#  - 200MB band = 2048*200 = 409,600
+#  - 512MB band = 2048*512 = 1,048,576
+#  - 1GB band = 2048*1000 = 2,048,000
+#
+# How many files at most would be uploaded to iCloud Drive in a scenario where
+# the disk is 500GB and the bands are 100MB? 500,000MB/100MB = 5,000 files.
+# Considering this 5000 files could realistically mean 1M+ files, that
+# significantly reduces the overall strain on iCloud Drive, while still making
+# it useful for development. An example from current usage: I currently
+# have 1,966,783 files in an iCloud Disk that is only 464 files in iCloud Drive,
+# with an disk configured as 300GB with 1GB bands; of course, bands are not the
+# only files that make up a sparse bundle disk, but it is generally safe to
+# say that the bands make up the majority of the file count. The main
+# calculation, then, comes down to how large should the bands be and how much
+# tolerance does the current Internet connection have for uploading a band for
+# even the smallest change to a file; a 500Mbps connection woudl then mean that
+# it can upload (500/8)=62.5MB per second. Of course, this is wildly
+# inefficient, but when dealing with a highly dynamic base of files and you have
+# both the iCloud storage and bandwidth available, in addition to a need to keep
+# a live copy of all work in sync, this is actually a pretty decent solution.
+#
+# In order for this to work with iCloud Drive, the generated disk name cannot
+# end with the usual ".sparsebundle" extension, as iCloud Drive will interpret
+# this as a single file and thus ignore the whole benefit of having a sparse
+# bundle. By simply renaming it to have another file extension, this lets iCloud
+# Drive see this sparse bundle as just a bunch of gibberish files that need to
+# be individually transferred. Disk Utility does not care if the disk name has
+# a random other name, as it can detect what type of disk it is when attempting
+# to mount it.
+#
+# TODO:
+#  - add validation
+#  - create within the ~/iCloud/Disks/ path ($ICLOUD_DIRECTORY_LONG)
+#  - tie into mounting
+#
+# @author Dane MacMillan <work@danemacmillan.com>
+icloud_disk()
+{
+	local command_name="icloud_disk"
+
+	local disk_name="${1}"
+
+	local disk_size_gb="1"
+	if [[ -n "${2}" ]]; then
+  	local disk_size_gb="${2}"
+  fi
+
+	local band_size_mb=$((2048 * 8))
+	if [[ -n "${3}" ]]; then
+		local band_size_mb=$((2048 * "${3}"))
+	fi
+
+	if [[ -n ${disk_name} && -n ${disk_size_gb} && -n ${band_size_mb} ]]; then
+		#hdiutil create -encryption AES-128 -type SPARSEBUNDLE -size 300g -fs "APFS" projects -imagekey sparse-band-size=2097152 -volname projects
+		echo -e "Creating encrypted sparsebundle for iCloud Disk with following command:"
+		local command="hdiutil create -encryption AES-128 -type SPARSEBUNDLE -size "${disk_size_gb}"g -fs "APFS" "${disk_name}" -imagekey sparse-band-size="${band_size_mb}" -volname "${disk_name}""
+		echo -e "  ${GREEN}${command}${RESET}"
+		${command}
+		mv "${disk_name}.sparsebundle" "${disk_name}.sparsebundle.iCloudDisk"
+		echo -e "Created iCloud Disk: ${disk_name}.sparsebundle.iCloudDisk"
+	else
+		echo -e "Calculate the MD5 hash of a file (or string if path not found)"
+		echo -e "Usage:\n  ${BCYAN}${command_name}${RESET} ${GREEN}DISK_NAME DISK_SIZE_GB BAND_SIZE_MB"
+		echo ""
+		echo -e "${RESET}Example usages:\n"
+		echo "  ${BCYAN}icloud_disk ${GREEN}foobar${RESET}  (create a disk named 'foobar' with a 1GB sparse bundle disk size and 8MB band size)"
+		echo "  ${BCYAN}icloud_disk ${GREEN}projects 500 200${RESET}  (create a disk named 'projects' with a 500GB sparse bundle disk size and 200MB band size)"
+		echo ""
+		return 1
+	fi
+}
 
 # Experimental sparsebundle sync.
 icloud_bundle_sync()
 {
-	"rsync" -aP --delete "${HOME}/projects.sparsebundle/" "${HOME}/iCloud/Disks/projects.sparsebundle.dir"
+	"rsync" -aP --delete "${HOME}/projects.sparsebundle/" "${HOME}/iCloud/Disks/projects.sparsebundle.iCloudDisk"
 }
 
 icloud_disk_mount_projects()
 {
-	local ICLOUD_DISK_PROJECTS="${ICLOUD_DIRECTORY_LONG}/Disks/projects.sparsebundle.dir"
+	local ICLOUD_DISK_PROJECTS="${ICLOUD_DIRECTORY_LONG}/Disks/projects.sparsebundle.iCloudDisk"
 	#local ICLOUD_DISK_PROJECTS="${HOME}/Disks/projects.sparsebundle.dir"
 
 	brctl download "${ICLOUD_DISK_PROJECTS}"
